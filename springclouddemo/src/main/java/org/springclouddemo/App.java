f2a0e804ef1a83b4a36cf20cf9b866c7c3448e4a
jungle0127 <arbor@foxmail.com>
2018-04-21 17:18:00 +0800

    added cloud project

---
+++ springclouddemo/src/main/java/org/springclouddemo/App.java
@@ -0,0 +1,11 @@
+package org.springclouddemo;
+
+import org.springframework.context.annotation.EnableMBeanExport;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
