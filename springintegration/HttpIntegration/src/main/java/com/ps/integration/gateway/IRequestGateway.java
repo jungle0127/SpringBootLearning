a7fe77cfb801bbf390c3e663f960e51de279c63b
jungle0127 <arbor@foxmail.com>
2018-07-20 17:03:17 +0800

    added basic http demo without compelement

---
+++ springintegration/HttpIntegration/src/main/java/com/ps/integration/gateway/IRequestGateway.java
@@ -0,0 +1,5 @@
+package com.ps.integration.gateway;
+
+public interface IRequestGateway {
+	String echo(String req);
+}
---
+++ springintegration/HttpIntegration/src/main/java/com/ps/integration/gateway/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.ps.integration.gateway;
