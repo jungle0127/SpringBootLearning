065a3321ac17a6cb0a67b99d1b0c4d61ff0faaa7
jungle0127 <arbor@foxmail.com>
2018-08-19 10:20:13 +0800

    change controlleradvice to restcontrolleradvice

--- FirstDemo/src/main/java/com/ps/demo/controller/intercept/ControllerExceptionHandler.java
+++ FirstDemo/src/main/java/com/ps/demo/controller/intercept/ControllerExceptionHandler.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.ps.demo.exception.UserInvalidException;
 
-@ControllerAdvice
+@RestControllerAdvice
+//@ControllerAdvice
 public class ControllerExceptionHandler {
 	@ExceptionHandler(UserInvalidException.class)
-	@ResponseBody
 	@ResponseStatus(code=HttpStatus.INTERNAL_SERVER_ERROR)
+	//@ResponseBody
 	public Map<String, Object> handleUserInvalidException(UserInvalidException ex){
 		System.out.println("Handler began....");
 		Map<String,Object> resultMap = new HashMap<>();
