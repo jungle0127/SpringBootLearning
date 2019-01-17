00c7792d51f20ccd92b73934f9682a6c54fd543d
jungle0127 <arbor@foxmail.com>
2019-01-17 23:57:09 +0800

    added configuration files for mybatis demo projects

---
+++ mybatis-demo/procedure-demo/src/main/java/com/ps/batis/procedure/ProcedureApplication.java
@@ -0,0 +1,11 @@
+package com.ps.batis.procedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcedureApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcedureApplication.class,args);
+    }
+}
