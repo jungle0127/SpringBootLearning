51e6446d3b602539cf13ba3a893e7651beb4de30
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2019-02-18 16:55:39 +0800

    updated slides

---
+++ startup-src-read/src/main/java/com/ps/srcread/startup/StartupApplication.java
@@ -0,0 +1,11 @@
+package com.ps.srcread.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartupApplication.class,args);
+    }
+}
---
+++ startup-src-read/src/main/java/com/ps/srcread/startup/runner/StartUpRunner.java
@@ -0,0 +1,20 @@
+package com.ps.srcread.startup.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartUpRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Startup runner begin............................");
+        StandardEnvironment env = new StandardEnvironment();
+        String[] profiles = env.getActiveProfiles();
+        System.out.println(profiles.length);
+        for(String profile: profiles){
+            System.out.println(profile);
+        }
+        System.out.println("Startup runner end..............................");
+    }
+}
